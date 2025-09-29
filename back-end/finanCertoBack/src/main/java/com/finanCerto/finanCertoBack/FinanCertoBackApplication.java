e12cbf489cac594b16880db7765a0c94e02344dd
Your Name <arthurlopes25072005@gmail.com>
2025-09-29 17:01:23 -0300

    feat: criado a base do projeto onde o back foi feito como o spring iniatializr e o banco de dados postgres atravez do docker

---
+++ back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/FinanCertoBackApplication.java
@@ -0,0 +1,13 @@
+package com.finanCerto.finanCertoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanCertoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanCertoBackApplication.class, args);
+	}
+
+}
---
+++ back-end/finanCertoBack/src/test/java/com/finanCerto/finanCertoBack/FinanCertoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanCerto.finanCertoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanCertoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
