3f13dfaa699d712dfa9d1dc4ff2d991a8eaf5479
Your Name <arthurlopes25072005@gmail.com>
2025-10-07 18:09:35 -0300

    feat: adicionando logs para o cadastro de categoria

--- back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/categoria/CategoriaService.java
+++ back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/categoria/CategoriaService.java
@@ -6,8 +6,13 @@ import com.finanCerto.finanCertoBack.security.TokenService;
 import com.finanCerto.finanCertoBack.usuario.Usuario;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 @Service
 public class CategoriaService {
+    private static final Logger logger = LoggerFactory.getLogger(CategoriaService.class);
     private final CategoriaRepository repository;
     private final TokenService tokenService;
 
@@ -16,13 +21,18 @@ public class CategoriaService {
         this.tokenService = tokenService;
     }
     public Categoria cadastro(CategoriaCadastroDto dto){
+        logger.info("tentando o cadastro da categoria: {}", dto.nome());
         Usuario usuario = tokenService.obterUsuario(dto.token());
+        logger.info("Recuperado o usuario pelo token: {}", usuario);
         if (repository.existsByUsuarioIdAndNome(usuario.getId(), dto.nome())){
+            logger.warn("Categoria com o mesmo nome ja criada: {}", dto.nome());
             throw  new CategoriaComMesmoNome("ja tem uma categoria com este nome");
         }
         Categoria categoria = new Categoria(dto);
         categoria.setUsuario(usuario);
+
         repository.save(categoria);
+        logger.info("Categoria salva no banco de dados");
         return categoria;
     }
 }
