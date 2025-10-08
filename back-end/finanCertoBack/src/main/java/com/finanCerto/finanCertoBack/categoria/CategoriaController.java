569b273bf0f94df08ca0659c613194cb5b6f331c
Your Name <arthurlopes25072005@gmail.com>
2025-10-08 15:18:42 -0300

    feat: colocando logs no categoria cadstro controller

--- back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/categoria/CategoriaController.java
+++ back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/categoria/CategoriaController.java
@@ -1,6 +1,8 @@
 package com.finanCerto.finanCertoBack.categoria;
 
 import com.finanCerto.finanCertoBack.categoria.dto.CategoriaCadastroDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/categorias")
 public class CategoriaController {
     private final  CategoriaService service;
+    private static final Logger logger = LoggerFactory.getLogger(CategoriaController.class);
 
     public CategoriaController(CategoriaService service) {
         this.service = service;
     }
     @PostMapping
     public ResponseEntity<Categoria> cadastro(@RequestBody CategoriaCadastroDto dto){
+        logger.info("Recebido a requisicao de cadastro de categoria: {}", dto.nome());
         Categoria categoria = service.cadastro(dto);
+        logger.info("Categoria cadastrada: id= {} , nome= {}",categoria.getId(),categoria.getNome());
         return ResponseEntity.status(HttpStatus.CREATED).body(categoria);
     }
 }
