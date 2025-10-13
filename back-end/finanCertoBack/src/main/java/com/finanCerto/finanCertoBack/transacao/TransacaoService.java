8658bfc88e63f3bfee7b79cb1f53791dc974ad3a
Your Name <arthurlopes25072005@gmail.com>
2025-10-13 15:04:15 -0300

    feat: colocando os metodos para adicionar valor no orcamento

--- back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/transacao/TransacaoService.java
+++ back-end/finanCertoBack/src/main/java/com/finanCerto/finanCertoBack/transacao/TransacaoService.java
@@ -5,6 +5,7 @@ import com.finanCerto.finanCertoBack.categoria.CategoriaService;
 import com.finanCerto.finanCertoBack.conta.Conta;
 import com.finanCerto.finanCertoBack.conta.ContaService;
 import com.finanCerto.finanCertoBack.exception.TransacaoNaoEncontrada;
+import com.finanCerto.finanCertoBack.orcamento.OrcamentoService;
 import com.finanCerto.finanCertoBack.security.TokenService;
 import com.finanCerto.finanCertoBack.transacao.dto.TransacaoCadastroDto;
 import com.finanCerto.finanCertoBack.usuario.Usuario;
@@ -20,13 +21,15 @@ public class TransacaoService {
     private final CategoriaService categoriaService;
     private final TokenService tokenService;
     private final ContaService contaService;
+    private final OrcamentoService orcamentoService;
     private static final Logger logger = LoggerFactory.getLogger(TransacaoService.class);
 
-    public TransacaoService(TransacaoRepository repository, CategoriaService categoriaService, TokenService tokenService, ContaService contaService) {
+    public TransacaoService(TransacaoRepository repository, CategoriaService categoriaService, TokenService tokenService, ContaService contaService, OrcamentoService orcamentoService) {
         this.repository = repository;
         this.categoriaService = categoriaService;
         this.tokenService = tokenService;
         this.contaService = contaService;
+        this.orcamentoService = orcamentoService;
     }
     public Transacao cadastro(TransacaoCadastroDto dto){
         logger.info("Tentando cadastrar transacao: {}",dto.descricao());
@@ -36,9 +39,11 @@ public class TransacaoService {
         Transacao transacao = new Transacao(dto,usuario,categoria,conta);
         if(transacao.getTipo() == Tipos.DESPESA){
             contaService.DiminuirSaldo(transacao.getValor(), transacao.getConta());
+            orcamentoService.adicionar(dto.valor(),categoria);
         }
         else if (transacao.getTipo() == Tipos.RECEITA){
             contaService.AdicionarSaldo(transacao.getValor(), transacao.getConta());
+            orcamentoService.adicionar(dto.valor(),categoria);
         }
         repository.save(transacao);
         logger.info("salva a transacao de descricao: {}",dto.descricao());
