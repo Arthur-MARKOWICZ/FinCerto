3e15bc564c0cc5d24923f7370089900776409aa3
Your Name <arthurlopes25072005@gmail.com>
2025-10-20 15:57:18 -0300

    feat: criando/arrumado a base do servico em python

--- back-end/financerto-analytics/app/main.py
+++ back-end/financerto-analytics/app/main.py
@@ -1,6 +1,13 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from app.database import get_db
+import sys
+
 
 app = FastAPI()
 
@@ -10,6 +17,6 @@ def home():
 
 @app.get("/test-db")
 def test_db(db: Session = Depends(get_db)):
-    result = db.execute("SELECT NOW();")
-    data = result.fetchone()
-    return {"db_time": data[0]}
+    result = db.execute(text("SELECT NOW();"))
+    data = result.scalar()
+    return {"db_time": data}
